fee2d20ffbca6b31960b5bd01fb7f4f00f305c3b
Robbie Palmer <Robbie-Palmer@users.noreply.github.com>
2026-02-04 19:25:48 +0000

    enable cloudflare pages build caching

--- infra/cloudflare/main.tf
+++ infra/cloudflare/main.tf
@@ -8,6 +8,7 @@ resource "cloudflare_pages_project" "personal_site" {
   production_branch = var.production_branch
 
   build_config {
+    build_caching   = true
     build_command   = "curl https://mise.run | sh && export PATH=\"$HOME/.local/bin:$PATH\" && MISE_IGNORED_CONFIG_PATHS=~/.tool-versions MISE_EXPERIMENTAL=1 mise run //ui:build"
     destination_dir = "ui/out"
     root_dir        = ""
