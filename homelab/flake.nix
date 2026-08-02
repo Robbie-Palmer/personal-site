fbe9a3e96f6f6146aa0234326aa7ed09f10b247d
Robbie Palmer <8760191+Robbie-Palmer@users.noreply.github.com>
2026-08-02 15:47:31 +0100

    feat: rename host to asus-desktop

--- homelab/flake.nix
+++ homelab/flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.gpu-worker = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.asus-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./hosts/gpu-worker ];
     };
